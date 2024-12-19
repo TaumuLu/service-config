4b96b4a8e52331c437b25d72c23c33bd4f868086
TaumuLu <taumux@gmail.com>
2024-12-19 13:53:32 +0800

    feat: echo tips

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,15 +6,16 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "-------------"
-echo "replace zshrc"
+echo "replace ~/.zshrc"
 echo "-------------"
 
 curl -s https://raw.githubusercontent.com/TaumuLu/service-config/refs/heads/master/zsh/.zshrc -o ~/.zshrc
 
+echo "-------------"
+echo "switch zsh"
+echo "-------------"
 zsh
 
 echo "-------------"
 echo "source ~/.zshrc"
 echo "-------------"
-
-# 修改配置后执行
